891d7644307e2a5f61b93e592a8ea11b586fa612
rahbu <113043422+rahbu@users.noreply.github.com>
2022-09-07 08:26:45 -0700

    1 file modified, 2 added

--- src/app/display.component.ts
+++ src/app/display.component.ts
@@ -2,8 +2,8 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'display',
-  template: `<h1> {{message.date}}</h1> <p>{{message.text}}</p>`,
-  styles: [`h1 { font-family: Lato; }`],
+  templateUrl: './display.component.html',
+  styleUrls: ['./display.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DisplayComponent {
